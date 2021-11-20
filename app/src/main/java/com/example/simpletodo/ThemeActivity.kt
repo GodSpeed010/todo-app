669b5ddf8d14c8b02026aa67e09098906532b186
Ahmad Hossain <ahmad.awaab@gmail.com>
2021-11-20 13:33:49 -0500

    theme can be changed once in ThemeActivity via button press

--- app/src/main/java/com/example/simpletodo/ThemeActivity.kt
+++ app/src/main/java/com/example/simpletodo/ThemeActivity.kt
@@ -1,25 +1,38 @@
 package com.example.simpletodo
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.graphics.drawable.Drawable
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.core.content.ContextCompat
 
 class ThemeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_theme)
 
-//
-//        findViewById<Button>(R.id.btn_red).setOnClickListener {
-//            //change theme to red
-//        }
-//        findViewById<Button>(R.id.btn_green).setOnClickListener {
-//
-//        }
-//        findViewById<Button>(R.id.btn_blue).setOnClickListener {
-//
-//        }
-//
+
+        findViewById<Button>(R.id.btn_red).setOnClickListener {
+            //change theme to red
+            setTheme(R.style.Theme_SimpleToDo_Red)
+            setContentView(R.layout.activity_theme)
+            supportActionBar?.setBackgroundDrawable(ColorDrawable(ContextCompat.getColor(this,R.color.red_800)))
+
+        }
+        findViewById<Button>(R.id.btn_green).setOnClickListener {
+            setTheme(R.style.Theme_SimpleToDo_Green)
+            setContentView(R.layout.activity_theme)
+            supportActionBar?.setBackgroundDrawable(ColorDrawable(ContextCompat.getColor(this,R.color.green_800)))
+        }
+        findViewById<Button>(R.id.btn_blue).setOnClickListener {
+            setTheme(R.style.Theme_SimpleToDo_Blue)
+            setContentView(R.layout.activity_theme)
+            supportActionBar?.setBackgroundDrawable(ColorDrawable(ContextCompat.getColor(this,R.color.blue_700)))
+
+        }
 
     }
 }
